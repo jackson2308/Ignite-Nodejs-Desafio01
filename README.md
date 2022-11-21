4306b4b1ed6f93e8b729abb40233fd019de8ba7a
Jackson Augusto <jacksonaugusto@yahoo.com.br>
2022-11-21 10:59:39 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">
+  <img src="https://ik.imagekit.io/ij3myhonv21/31S0Ht7jUyL_3LcU09hEQ.png">
+</h1>
+
+# 📖 Sobre
+
+Segundo desafio da trilha **NodeJS** do **Ignite Pro**. Nesse desafio vimos mais detalhado os middlewares no Express.
+
+Será permitida a criação de um usuário com name e username, bem como fazer o CRUD de todos:
+
+Criar um novo todo;
+Listar todos os todos;
+Alterar o title e deadline de um todo existente;
+Marcar um todo como feito;
+Excluir um todo;
+Tudo isso para cada usuário em específico. Além disso, dessa vez tem um plano grátis onde o usuário só pode criar até dez todos e um plano Pro que irá permitir criar todos ilimitados, isso tudo usando middlewares para fazer as validações necessárias.
+
+# 👨‍💻 Tecnologias utilizadas
+
+Nesse projeto utilizamos as seguintes tecnologias:
+
+[![Node Badge](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white&link=https://nodejs.org/en/)](https://nodejs.org/en/)
+[![Express Badge](https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white&link=https://expressjs.com/pt-br/)](https://expressjs.com/pt-br/)
+
+
+# ⚙️ Get Started
+
+```bash
+  # Clone o Repositório:
+  $ git clone
+   https://github.com/andrecampestre/Ignite-Nodejs-Desafio02
+
+  # Entre no Diretório:
+  $ cd Ignite-Nodejs-Desafio02
+
+  # Instale as Dependências do Projeto:
+  $ yarn install
+
+  # Testes:
+  $ yarn test
+  
+  # Inicie:
+  $ yarn dev
+```
--- src/index.js
+++ src/index.js
@@ -1,41 +1,123 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  
+  let userExist = users.some(user => user.username === username);
+  if (!userExist) return response.status(404).send({error: 'Usuário não encontrado'})
+  let userData = users.find(user => user.username === username )
+  request.user = userData
+  next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post("/users", (request, response) => {
+  const { name } = request.body;
+
+  const { username } = request.body;
+
+  const existUser = users.some(user => user.username === username)
+  if(existUser) return response.status(400).json({error: 'username já está em uso!'})
+
+  const user = {
+    id: uuidv4(),
+    name:name,
+    username: username,
+    todos: []
+  };
+
+  users.push(user);
+
+  response.status(201).json(user);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  
+  response.json(user.todos)
+  
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+
+  const { user } = request;
+
+  const { title } = request.body;
+
+  const { deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  response.status(201).json(todo);
+
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+   const { user } = request;
+
+   const { title } = request.body;
+
+   const { deadline } = request.body;
+
+   const { id } = request.params;
+
+   user.todos.forEach(todo => {
+      if (todo.id === id) {
+        todo.title = title;
+        todo.deadline = new Date(deadline);
+        return response.json(todo);
+      }
+  });
+
+   return response.status(404).json({error: 'Tarefa não encontrada'});
+
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  const { id } = request.params;
+
+   user.todos.forEach(todo => {
+      if (todo.id === id) {
+        todo.done = true;
+        return response.json(todo);
+      }
+  });
+
+   return response.status(404).json({error: 'Tarefa não encontrada'});
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  const { id } = request.params;
+
+   user.todos.forEach(todo => {
+      if (todo.id === id) {
+        user.todos.splice(todo, 1)
+        return response.status(204).json()
+      }
+  });
+
+   return response.status(404).json({error: 'Tarefa não encontrada'});
 });
 
 module.exports = app;
